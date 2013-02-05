9e997a2c85e6fb005592ffaa2dd10cc9c6708105
Andrew Couch <andy@couchand.com>
2013-02-05 13:43:03 -0500

    initial import of "legacy" app

---
+++ test/config.js
@@ -0,0 +1,63 @@
+(function() {
+
+  // test mode -- 'tdd', 'bdd-should', or 'bdd-expect'
+  var testMode = 'tdd';
+  var testModes = {
+    'tdd': function() {
+      window.assert = chai.assert;
+    },
+    'bdd-should': function() {
+      window.should = chai.should();
+    },
+    'bdd-expect': function() {
+      window.expect = chai.expect;
+    }
+  };
+
+  testModes[testMode] && testModes[testMode]();
+
+  require.config({
+    // Interpret module names in terms of the application's base directory.
+    // This allows us to use the application configuration data without
+    // modification (see below).
+    baseUrl: "../../www/js",
+    paths: {
+      templates: '../templates',
+      'jquery.simulate': '../lib/jquery.simulate',
+      tests: '../../test/tests',
+      fixtures: '../../test/fixtures',
+      squire: '../lib/squire'
+    },
+
+    shim: {
+      'jquery.simulate': {
+        deps: [ 'jquery' ]
+      }
+    }
+  });
+
+  // Define a fake "main" module to prevent Require.js from loading the app's
+  // true "main" module (which is unecessary for unit testing)
+  define("main", function() {});
+
+  mocha.setup({
+    ui: testMode.split('-')[0],
+    globals: [ 'XMLHttpRequest' ]
+  });
+
+  require([
+    // Load the app's Require.js configuration file
+    '../../www/js/config',
+    // Load the list of test files
+    '../../test/list_of_tests'
+    ], function(config, listOfTests) {
+
+      // Load the test files themselves
+      require( listOfTests, function() {
+
+        // Initial the tests!
+        mocha.run();
+      });
+  });
+
+}());
---
+++ www/js/main.js
@@ -0,0 +1,53 @@
+var tmplCache = {};
+
+function loadTemplate(name) {
+  if ( !tmplCache[ name ] ) {
+    tmplCache[ name ] = $.get( '/templates/' + name );
+  }
+  return tmplCache[ name ];
+}
+
+$(function() {
+
+  var resultsList = $( '#results' );
+  var liked = $( '#liked' );
+  var pending = false;
+
+  $( '#searchForm' ).on( 'submit', function( e ) {
+    e.preventDefault();
+
+    if ( pending ) { return; }
+
+    var form = $( this );
+    var query = $.trim( form.find( 'input[name="q"]' ).val() );
+
+    if ( !query ) { return; }
+
+    pending = true;
+
+    $.ajax( '/data/search.json', {
+      data : { q: query },
+      dataType : 'json',
+      success : function( data ) {
+        loadTemplate('people-detailed.tmpl').then(function(t) {
+          var tmpl = _.template( t );
+          resultsList.html( tmpl({ people : data.results }) );
+          pending = false;
+        });
+      }
+    });
+
+    $('<li>', {
+      'class' : 'searching',
+      html : 'Searching &hellip;'
+    }).appendTo( resultsList.empty() );
+  });
+
+  resultsList.on( 'click', '.like', function(e) {
+    e.preventDefault();
+    var name = $( this ).closest( 'li' ).find( 'h2' ).text();
+    liked.find( '.no-results' ).remove();
+    $( '<li>', { text: name } ).appendTo( liked );
+  });
+
+});
