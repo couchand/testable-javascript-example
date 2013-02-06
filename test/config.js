439882ec52b21e8b9bf60758d1fdabbaa3401d3c
Andrew Couch <andy@couchand.com>
2013-02-06 14:08:04 -0500

    test config: switch to expect style

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 (function() {
 
   // test mode -- 'tdd', 'bdd-should', or 'bdd-expect'
-  var testMode = 'bdd-should';
+  var testMode = 'bdd-expect';
   var testModes = {
     'tdd': function() {
       window.assert = chai.assert;
