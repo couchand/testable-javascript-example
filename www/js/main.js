f71ec7a3b9484f7ef4892ee41a452160c014ac6e
Andrew Couch <andy@couchand.com>
2013-02-05 20:33:50 -0500

    break out application controller

--- www/js/main.js
+++ www/js/main.js
@@ -1,42 +1,20 @@
 require([
-  'template-store',
-  'people',
   'likes-list',
   'results-list',
-  'semaphore',
   'search-form',
+  'application-controller',
   'jquery'
-], function( TemplateStore, People, LikesList, ResultsList, Semaphore, SearchForm, $ ) {
-
-  var templates = new TemplateStore();
-  var pending = new Semaphore();
+], function( LikesList, ResultsList, SearchForm, ApplicationController, $ ) {
 
   $(function() {
     var results = new ResultsList( '#results' );
     var likes = new LikesList( '#liked' );
     var searchForm = new SearchForm( '#searchForm' );
+    var controller = new ApplicationController();
 
-    $( document ).on( 'search', function( e, name ) {
-      if ( !pending.raise() ) { return; }
-
-      var peopleFound = People.findByName( name );
-      var templatesFetched = templates.fetch('people-detailed.tmpl');
-
-      $.when(
-        peopleFound,
-        templatesFetched
-      ).done(
-        $.proxy( results, 'update' )
-      ).always(
-        $.proxy( pending, 'lower' )
-      );
-
-      results.block();
-    });
-
-    $( document ).on('like', function( e, person ) {
-      likes.add( person );
-    });
+    controller.registerResults( results );
+    controller.registerLikes( likes );
+    controller.registerSearch( searchForm );
 
   });
 
